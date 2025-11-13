9599d4b82ae7e2dbce860a50a098a1baca9d9184
Tnalxmsk <alstnwkd990@naver.com>
2025-11-14 01:49:18 +0900

    :sparkles: feat: 기본 route, theme Provider 적용
    App 컴포넌트 내 Provider 주입

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import './shared/styles/reset.css.ts';
+import './shared/styles/global.css.ts';
+import './shared/styles/font.css.ts';
+import { AppRouterProvider, AppThemeProvider } from "./app/providers";
+
 function App() {
-  return <></>;
+  return (
+    <AppThemeProvider>
+      <AppRouterProvider />
+    </AppThemeProvider>
+  );
 }
 
 export default App;
