1426b65c172fc8711f952820747c2beaadedbf2f
bhyerain <seunghye0307@naver.com>
2025-11-14 10:35:19 +0900

    🔧chore: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
 });
