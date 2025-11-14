2eb66433831f8a9412a8eb720e6710fa92391f89
bhyerain <seunghye0307@naver.com>
2025-11-14 12:32:21 +0900

    🔧chore: svgr 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
