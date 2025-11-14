95dcad0f1f8f1794ecf0d935a187825890075843
bhyerain <seunghye0307@naver.com>
2025-11-15 01:49:10 +0900

    🎨fix: 경로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
-import { QueryProvider } from "./app/providers/QueryProvider.tsx";
+import { QueryProvider } from "@app/providers/QueryProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
