49f8b196e45c09245c44ad183eaaa9c174f541e4
임지수 <dlawltn0708@naver.com>
2025-11-16 17:54:00 +0900

    ✨ feat: 매거진 카드 컴포넌트 구현

---
+++ src/widgets/cards/magazineCard/ui/MagazineCard.tsx
@@ -0,0 +1,22 @@
+import * as s from "./MagazineCard.css";
+
+interface MagazineCardProps {
+  imageUrl: string;
+  alt: string;
+}
+
+const MagazineCard = ({ imageUrl, alt }: MagazineCardProps) => {
+  return (
+    <div className={s.container}>
+      <img
+        className={s.image}
+        src={imageUrl}
+        alt={alt}
+        loading="lazy"
+        draggable={false}
+      />
+    </div>
+  );
+};
+
+export default MagazineCard;
