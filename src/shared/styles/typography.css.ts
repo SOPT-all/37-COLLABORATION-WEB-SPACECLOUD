ee02caef6f786be9d1164ebfd7791b8edf2871e1
Tnalxmsk <alstnwkd990@naver.com>
2025-11-16 19:01:33 +0900

    :recycle: refactor: 폰트 통합 수정사항 반영

--- src/shared/styles/typography.css.ts
+++ src/shared/styles/typography.css.ts
@@ -4,116 +4,116 @@ import { vars } from '@shared/styles/token.css.ts';
 export const typography = styleVariants({
   // ===== Head =====
   head_b_24: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.bold,          // Bold
-    fontSize: vars.fontSize['24'],             // 24px
-    lineHeight: vars.lineHeight['28'],         // 28
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.bold,
+    fontSize: vars.font.size.s24,
+    lineHeight: vars.font.lineHeight.h28,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   head_sb_20: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.semibold,      // SemiBold
-    fontSize: vars.fontSize['20'],             // 20px
-    lineHeight: vars.lineHeight['20'],         // 20
-    letterSpacing: vars.letterSpacing.tight_05, // -0.5%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.semibold,
+    fontSize: vars.font.size.s20,
+    lineHeight: vars.font.lineHeight.h20,
+    letterSpacing: vars.font.letterSpacing.tight_05,
   },
 
   // ===== Body =====
   body_sb_18: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.semibold,      // Semibold
-    fontSize: vars.fontSize['18'],             // 18px
-    lineHeight: vars.lineHeight['20'],         // 20
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.semibold,
+    fontSize: vars.font.size.s18,
+    lineHeight: vars.font.lineHeight.h20,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_m_18: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.medium,        // Medium
-    fontSize: vars.fontSize['18'],             // 18px
-    lineHeight: vars.lineHeight['20'],         // 20
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.medium,
+    fontSize: vars.font.size.s18,
+    lineHeight: vars.font.lineHeight.h20,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_sb_16: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.semibold,      // Semibold
-    fontSize: vars.fontSize['16'],             // 16px
-    lineHeight: vars.lineHeight['24'],         // 24
-    letterSpacing: vars.letterSpacing.tight_04, // -0.4%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.semibold,
+    fontSize: vars.font.size.s16,
+    lineHeight: vars.font.lineHeight.h24,
+    letterSpacing: vars.font.letterSpacing.tight_04,
   },
 
   body_m_16: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.medium,        // Medium
-    fontSize: vars.fontSize['16'],             // 16px
-    lineHeight: vars.lineHeight['18'],         // 18
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.medium,
+    fontSize: vars.font.size.s16,
+    lineHeight: vars.font.lineHeight.h18,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_sb_14: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.semibold,      // Semibold
-    fontSize: vars.fontSize['14'],             // 14px
-    lineHeight: vars.lineHeight['16'],         // 16
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.semibold,
+    fontSize: vars.font.size.s14,
+    lineHeight: vars.font.lineHeight.h16,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   // 이미지 스펙 기준: 이름은 14지만 Font Size는 12px로 설정되어 있음
   body_m_14: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.medium,        // Medium
-    fontSize: vars.fontSize['12'],             // 12px
-    lineHeight: vars.lineHeight['14'],         // 14
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.medium,
+    fontSize: vars.font.size.s12,
+    lineHeight: vars.font.lineHeight.h14,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_sb_12: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.semibold,      // Semibold
-    fontSize: vars.fontSize['12'],             // 12px
-    lineHeight: vars.lineHeight['16'],         // 16
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.semibold,
+    fontSize: vars.font.size.s12,
+    lineHeight: vars.font.lineHeight.h16,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_r_13: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.regular,       // Regular
-    fontSize: vars.fontSize['13'],             // 13px
-    lineHeight: vars.lineHeight['18'],         // 18
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.regular,
+    fontSize: vars.font.size.s13,
+    lineHeight: vars.font.lineHeight.h18,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_m_12: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.medium,        // Medium
-    fontSize: vars.fontSize['12'],             // 12px
-    lineHeight: vars.lineHeight['14'],         // 14
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.medium,
+    fontSize: vars.font.size.s12,
+    lineHeight: vars.font.lineHeight.h14,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_r_12: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.regular,       // Regular
-    fontSize: vars.fontSize['12'],             // 12px
-    lineHeight: vars.lineHeight['18'],         // 18
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.regular,
+    fontSize: vars.font.size.s12,
+    lineHeight: vars.font.lineHeight.h18,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_r_12_2: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.regular,       // Regular
-    fontSize: vars.fontSize['12'],             // 12px
-    lineHeight: vars.lineHeight['14'],         // 14
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.regular,
+    fontSize: vars.font.size.s12,
+    lineHeight: vars.font.lineHeight.h14,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 
   body_r_10: {
-    fontFamily: vars.font.pretendard,
-    fontWeight: vars.fontWeight.regular,       // Regular
-    fontSize: vars.fontSize['10'],             // 10px
-    lineHeight: vars.lineHeight['12'],         // 12
-    letterSpacing: vars.letterSpacing.tight_03, // -0.3%
+    fontFamily: vars.font.family.pretendard,
+    fontWeight: vars.font.weight.regular,
+    fontSize: vars.font.size.s10,
+    lineHeight: vars.font.lineHeight.h12,
+    letterSpacing: vars.font.letterSpacing.tight_03,
   },
 });
