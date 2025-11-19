9c06e6816327ab0ff83e38a17059869f154b2568
bhyerain <seunghye0307@naver.com>
2025-11-19 18:48:01 +0900

    ✨ feat: 헤더 컴포넌트 폴더 구조 세팅 및 구현

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as ChatIcon } from './ic-chat.svg?react';
+export { default as MenuIcon } from './ic-menu.svg?react';
+export { default as SearchIcon } from './ic-search.svg?react';
