c3ccf2954ea74523463e270f15a69a170e7a5364
bhyerain <seunghye0307@naver.com>
2025-11-20 00:28:09 +0900

    🔥chore: import 제거

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from 'react-router';
 import { rootLayout } from '@app/layouts/RootLayout.css.ts';
-import Header from '@/widget/main-view/ui/Header/Header';
 
 const RootLayout = () => {
   return (
     <>
       {/* 추후 구현한 헤더 컴포넌트를 추가합니다. */}
-      <Header />
+      <header />
       {/* 추후 루트 레이아웃 스타일링을 진행합니다. */}
       <main className={rootLayout}>
         <Outlet />
