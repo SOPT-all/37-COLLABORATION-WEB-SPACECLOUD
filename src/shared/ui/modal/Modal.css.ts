cd2cb388f6bb071de9e60e98fba72674cba268db
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-20 22:17:30 +0900

    ✨ feat: border variants 추가

--- src/shared/ui/modal/Modal.css.ts
+++ src/shared/ui/modal/Modal.css.ts
@@ -1,4 +1,5 @@
 import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 import { radiusToken, colorToken } from '@/shared/styles/tokens';
 
 export const overlay = style({
@@ -13,13 +14,28 @@ export const overlay = style({
   backgroundColor: colorToken.opacity.dim,
 });
 
-export const content = style({
-  backgroundColor: '#ffffff',
-  width: 'fit-content',
-  height: 'fit-content',
-  maxWidth: '69dvw',
-  maxHeight: '65dvh',
-  overflow: 'auto',
-  borderRadius: radiusToken.r4,
-  flexShrink: 0,
+export const content = recipe({
+  base: {
+    backgroundColor: '#ffffff',
+    width: 'fit-content',
+    height: 'fit-content',
+    maxWidth: '69dvw',
+    maxHeight: '65dvh',
+    overflow: 'auto',
+    borderRadius: radiusToken.r4,
+    flexShrink: 0,
+    '::-webkit-scrollbar': {
+      display: 'none',
+    },
+  },
+  variants: {
+    border: {
+      none: {
+        border: 'none',
+      },
+      gray300: {
+        border: `1px solid ${colorToken.grayscale.gray300}`,
+      },
+    },
+  },
 });
