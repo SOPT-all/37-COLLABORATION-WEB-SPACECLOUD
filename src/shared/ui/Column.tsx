d23fe4e41b1252ce994f4692f0e0bc6bcf56d7e2
Tnalxmsk <alstnwkd990@naver.com>
2025-11-20 13:19:24 +0900

    :sparkles: feat: className prop 추가

--- src/shared/ui/Column.tsx
+++ src/shared/ui/Column.tsx
@@ -1,18 +1,25 @@
 import { columnWrapper } from '@shared/ui/Column.css.ts';
 import type { ChildrenProps } from '@shared/types/common.ts';
 import type { PartialVariants } from '@shared/types/variants.ts';
+import clsx from 'clsx';
 
-type ColumnProps = ChildrenProps & PartialVariants<typeof columnWrapper>;
+type ColumnProps = ChildrenProps &
+  PartialVariants<typeof columnWrapper> & {
+    className?: string;
+  };
 
-const Column = ({ align, justify, children, width, height }: ColumnProps) => {
+const Column = ({ align, justify, children, width, height, className }: ColumnProps) => {
   return (
     <div
-      className={columnWrapper({
-        align: align,
-        justify: justify,
-        width: width,
-        height: height,
-      })}
+      className={clsx(
+        columnWrapper({
+          align: align,
+          justify: justify,
+          width: width,
+          height: height,
+        }),
+        className,
+      )}
     >
       {children}
     </div>
