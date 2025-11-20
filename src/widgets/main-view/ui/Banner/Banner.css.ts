540faa4d197a2759d93436fb5d67116a2ff89745
bhyerain <seunghye0307@naver.com>
2025-11-20 16:40:30 +0900

    ♻️refactor: 스타일 요소 추가 및 변경

--- src/widgets/main-view/ui/Banner/Banner.css.ts
+++ src/widgets/main-view/ui/Banner/Banner.css.ts
@@ -1,10 +1,10 @@
 import { style } from '@vanilla-extract/css';
-import { colorToken, radiusToken } from '@/shared/styles/tokens';
+import { colorToken, radiusToken, zIndexToken } from '@/shared/styles/tokens';
 import { typography } from '@/shared/styles/typography.css';
 
 export const wrapper = style({
   position: 'relative',
-  width: '1128px',
+  width: '1129px',
   height: '200px',
 });
 
@@ -12,7 +12,7 @@ export const container = style({
   width: '1128px',
   height: '200px',
   borderRadius: radiusToken.r12,
-  boxShadow: '0px 1px 8px 0px #0000001A',
+  boxShadow: '0px 1px 8px rgba(0, 0, 0, 0.1)',
   overflow: 'hidden',
   position: 'relative',
 });
@@ -25,12 +25,12 @@ export const counter = style({
   position: 'absolute',
   bottom: '36px',
   right: '122px',
-  backgroundColor: 'rgba(118, 118, 118, 0.5)',
+  backgroundColor: colorToken.grayscale.gray70050,
   width: '45px',
   height: '16px',
   borderRadius: radiusToken.r50,
-  zIndex: 999,
-  paddingTop: '1px',
+  zIndex: zIndexToken.z900,
+  paddingTop: '2px',
 });
 
 export const counterFont = style([
@@ -38,22 +38,21 @@ export const counterFont = style([
   {
     color: colorToken.grayscale.white,
     textAlign: 'center',
-    paddingTop: '1px',
   },
 ]);
 
 export const leftArrow = style({
   position: 'absolute',
   top: '50%',
-  left: '-15px',
-  transform: 'translateY(-50%)',
-  zIndex: 1000,
+  left: 0,
+  transform: 'translate(-50%, -50%)',
+  zIndex: zIndexToken.z1000,
 });
 
 export const rightArrow = style({
   position: 'absolute',
   top: '50%',
-  right: '-15px',
-  transform: 'translateY(-50%)',
-  zIndex: 1000,
+  right: 0,
+  transform: 'translate(50%, -50%)',
+  zIndex: zIndexToken.z1000,
 });
