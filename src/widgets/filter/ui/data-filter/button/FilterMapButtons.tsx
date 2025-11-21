563cdf66991869ea80ec8888c23d4d75ce838d2c
Tnalxmsk <alstnwkd990@naver.com>
2025-11-21 22:38:01 +0900

    :sparkles: feat: 데이터 필터 컴포넌트 트리거 로직 추가

--- src/widgets/filter/ui/data-filter/button/FilterMapButtons.tsx
+++ src/widgets/filter/ui/data-filter/button/FilterMapButtons.tsx
@@ -1,27 +1,28 @@
 import Button from '@shared/ui/Button.tsx';
-import { overlay } from 'overlay-kit';
 import { IcFilter, IcMap } from '@shared/assets/icons/space-types';
-import Row from '@shared/ui/Row.tsx';
+import Row from '@shared/ui/layout/Row.tsx';
 import { vars } from '@shared/styles/token.css.ts';
+import { useModal } from '@shared/libs/useModal.tsx';
+import DataFilter from '@widgets/filter/ui/data-filter/DataFilter.tsx';
 
 const FilterMapButtons = () => {
-  const openFilter = () =>
-    overlay.open(({ isOpen, close }) => (
-      <dialog open={isOpen} onClose={close} closedby='any'>
-        데이터 필터
-      </dialog>
-    ));
+  const { openModal } = useModal();
 
   const openMap = () => undefined;
   return (
     <Row gap={vars.space.s4} width='min'>
-      <Button onClick={openFilter} styleType='dataFilter' font='body_sb_16' gap='g4'>
-        <IcFilter />
+      <Button
+        onClick={() => openModal(<DataFilter />)}
+        styleType='dataFilter'
+        font='body_sb_16'
+        gap='g4'
+      >
+        <IcFilter aria-hidden={true} />
         <span>필터</span>
       </Button>
       <Button onClick={openMap} styleType='dataFilter' font='body_sb_16' gap='g4'>
-        <IcMap />
-        <span>필터</span>
+        <IcMap aria-hidden={true} />
+        <span>지도</span>
       </Button>
     </Row>
   );
