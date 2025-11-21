c6a19f1ab2170289461a1128f0ab0ed1c85faf47
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-22 01:34:16 +0900

    ✨ feat: 필터 상태 업데이트 함수 연결

--- src/widgets/space-filter/ui/modal/CapacityModalContent.tsx
+++ src/widgets/space-filter/ui/modal/CapacityModalContent.tsx
@@ -2,24 +2,40 @@ import Button from '@/shared/ui/Button';
 import { MinusIcon, PlusIcon } from '@/shared/assets/icons';
 import * as s from './CapacityModalContent.css.ts';
 import type { ModalProps } from '@/shared/types/common.ts';
+import { useEffect, useState } from 'react';
 
-const CapacityModalContent = ({ onClose }: ModalProps) => {
+const CapacityModalContent = ({ onClose, onChange }: ModalProps) => {
+  const [count, setCount] = useState(1);
+
+  const handleClick = (value: string) => {
+    onChange?.(value);
+    onClose?.();
+  };
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
   return (
     <div className={s.wrapper}>
       <div className={s.top}>
         <h3 className={s.label}>인원</h3>
         <div className={s.counter}>
-          <Button styleType='counter'>
+          <Button
+            styleType='counter'
+            onClick={() => setCount((prev) => prev - 1)}
+            disabled={count === 1}
+          >
             <MinusIcon width={10} height={2} />
           </Button>
-          <span className={s.num}>6</span>
-          <Button styleType='counter'>
+          <span className={s.num}>{count}</span>
+          <Button styleType='counter' onClick={() => setCount((prev) => prev + 1)}>
             <PlusIcon width={10} height={10} />
           </Button>
         </div>
       </div>
       <div className={s.bottom}>
-        <Button styleType='cta' font='body_sb_16' onClick={onClose}>
+        <Button styleType='cta' font='body_sb_16' onClick={() => handleClick(count.toString())}>
           <span>인원수 적용하기</span>
         </Button>
       </div>
