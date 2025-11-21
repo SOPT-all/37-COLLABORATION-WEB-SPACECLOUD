bb7d381d21632743a1abede74fe3c413bba08ef5
Tnalxmsk <alstnwkd990@naver.com>
2025-11-21 22:13:57 +0900

    :recycle: refactor: QueryProvider App.tsx로 import 이동

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
-import { QueryProvider } from "@app/providers/QueryProvider.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app/App.tsx';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryProvider>
-      <App />
-    </QueryProvider>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 );
