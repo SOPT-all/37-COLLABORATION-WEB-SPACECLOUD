335c5b8ba390f8b2895e2f55411e3093c3d597e9
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-21 13:42:52 +0900

    ♻️ refactor: border 타입 변경
    - PartialVariants 사용
    - clsx 사용

--- src/shared/ui/modal/Modal.tsx
+++ src/shared/ui/modal/Modal.tsx
@@ -1,17 +1,21 @@
 import type { CSSProperties } from 'react';
 import type { ChildrenProps, ModalProps } from '@/shared/types/common';
 import * as s from './Modal.css';
+import type { PartialVariants } from '@/shared/types/variants';
+import clsx from 'clsx';
 
-interface Props extends ChildrenProps, ModalProps {
-  location?: CSSProperties;
+type Border = PartialVariants<typeof s.content> & {
   border: 'none' | 'gray300';
+};
+interface Props extends ChildrenProps, ModalProps, Border {
+  location?: CSSProperties;
 }
 
 export const Modal = ({ onClose, children, location, border }: Props) => {
   return (
     <div className={s.overlay} onClick={onClose}>
       <section
-        className={s.content({ border: `${border}` })}
+        className={clsx(s.content({ border: border }))}
         onClick={(e) => e.stopPropagation()}
         style={location}
       >
