7bd42adbebcb9a25e189613e9b3a316d7761097e
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-22 01:22:23 +0900

    ✨ feat: onChange 추가

--- src/shared/types/common.ts
+++ src/shared/types/common.ts
@@ -2,7 +2,11 @@ import * as React from 'react';
 
 export type ChildrenProps = { children: React.ReactNode };
 
+// refactor
+export type SpaceFilterValue = { key: string | null; content: string };
+
 export interface ModalProps {
   isOpen?: boolean;
   onClose?: () => void;
+  onChange?: (value: string | SpaceFilterValue) => void;
 }
