69f3076a295f7e9d85e47646694fcbe6fb2fe6c9
Tnalxmsk <alstnwkd990@naver.com>
2025-11-21 22:29:42 +0900

    :sparkles: feat: 편의 시설 필터 컴포넌트 추가

---
+++ src/widgets/filter/ui/data-filter/amenity/AmenityFilter.css.ts
@@ -0,0 +1,31 @@
+import { recipe } from '@vanilla-extract/recipes';
+import { vars } from '@shared/styles/token.css.ts';
+
+export const amenityItemBase = recipe({
+  base: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: `${vars.space.s12} ${vars.space.s16}`,
+    borderRadius: vars.radius.r20,
+    backgroundColor: vars.color.grayscale.white,
+    color: vars.color.grayscale.gray800,
+    whiteSpace: 'nowrap',
+    cursor: 'pointer',
+  },
+
+  variants: {
+    active: {
+      false: {
+        boxShadow: `0 0 0 1px ${vars.color.grayscale.gray300}`,
+      },
+      true: {
+        boxShadow: `0 0 0 1px ${vars.color.primary[200]}`,
+        color: vars.color.primary[200],
+      },
+    },
+  },
+
+  defaultVariants: {
+    active: false,
+  },
+});
