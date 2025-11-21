b1f075f0645c0724b93d8d1fd5f8e805f183bf67
임지수 <dlawltn0708@naver.com>
2025-11-21 14:09:56 +0900

    ♻️ refactor: mock 데이터 제거

--- src/widgets/space-card/ui/PlaceSection.tsx
+++ src/widgets/space-card/ui/PlaceSection.tsx
@@ -9,55 +9,13 @@ type PlaceSubTitleVariant = Extract<SubTitleVariant, 'small24' | 'large'>;
 interface PlaceSectionProps {
   title: string;
   subtitleVariant?: PlaceSubTitleVariant;
-  places?: PlaceCardProps[];
+  places: PlaceCardProps[];
 }
-
-const mockPlaces: PlaceCardProps[] = [
-  {
-    id: 1,
-    name: '이랑 한식주점배경촬영장',
-    capacity: 30,
-    commentCount: 0,
-    likeCount: 0,
-    price: 33000,
-    priceUnit: 'HOUR',
-    thumbnailImageUrl: 'https://placehold.co/362x162',
-    hashtags: ['한식주점', '촬영대관', '자연채광'],
-    address: '문정동',
-    coupon: false,
-  },
-  {
-    id: 2,
-    name: '[신촌]조용한소모임. 북카페',
-    capacity: 20,
-    commentCount: 0,
-    likeCount: 1,
-    price: 2500,
-    priceUnit: 'HOUR',
-    thumbnailImageUrl: 'https://placehold.co/362x162',
-    hashtags: ['공영주차장', '소모임', '독서모임', '#북카페'],
-    address: '노고산동',
-    coupon: false,
-  },
-  {
-    id: 3,
-    name: '마고',
-    capacity: 30,
-    commentCount: 0,
-    likeCount: 0,
-    price: 33000,
-    priceUnit: 'HOUR',
-    thumbnailImageUrl: 'https://placehold.co/362x162',
-    hashtags: ['운동', '촬영', '필라테스', '요가'],
-    address: '홍은동',
-    coupon: true,
-  },
-];
 
 const PlaceSection = ({
   title,
   subtitleVariant = 'small24',
-  places = mockPlaces,
+  places,
 }: PlaceSectionProps) => {
   const listRef = useRef<HTMLDivElement | null>(null);
   const [canScrollLeft, setCanScrollLeft] = useState(false);
