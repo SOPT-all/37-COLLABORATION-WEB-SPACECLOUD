09d24242b0a1597f8aabd4c26ba3cafac3a515ab
Tnalxmsk <alstnwkd990@naver.com>
2025-11-24 20:39:04 +0900

    :recycle: refactor: 장소 선택 모달 구조 개선
    - 목 데이터, 타입 분리
    - typedEntries 사용하여 타입 단언 제거

--- src/widgets/space-filter/ui/modal/SpaceModalContent.tsx
+++ src/widgets/space-filter/ui/modal/SpaceModalContent.tsx
@@ -2,138 +2,19 @@ import type { ModalProps } from '@/shared/types/common';
 import { SPACE_ICON_MAP } from '@/shared/configs/space';
 import Button from '@/shared/ui/Button';
 import * as s from './SpaceModalContent.css';
-
-// api 응답값
-const dummy = {
-  workspace: [
-    {
-      code: 'STUDY_ROOM',
-      name: '스터디룸',
-    },
-    {
-      code: 'MEETING_ROOM',
-      name: '회의실',
-    },
-    {
-      code: 'PRIVATE_OFFICE',
-      name: '독립오피스',
-    },
-    {
-      code: 'COWORKING_OFFICE',
-      name: '코워킹오피스',
-    },
-    {
-      code: 'RENTAL_STUDIO',
-      name: '렌탈스튜디오',
-    },
-    {
-      code: 'PHOTO_STUDIO',
-      name: '촬영스튜디오',
-    },
-    {
-      code: 'HORIZON',
-      name: '호리존',
-    },
-    {
-      code: 'OUTDOOR_SHOOT',
-      name: '실외촬영',
-    },
-    {
-      code: 'RECORDING_ROOM',
-      name: '녹음실',
-    },
-    {
-      code: 'PRACTICE_ROOM',
-      name: '연습실',
-    },
-    {
-      code: 'VOCAL_PRACTICE',
-      name: '보컬연습실',
-    },
-    {
-      code: 'INSTRUMENT_PRACTICE',
-      name: '악기연습실',
-    },
-    {
-      code: 'DANCE_PRACTICE',
-      name: '댄스연습실',
-    },
-    {
-      code: 'LIVE_STREAM',
-      name: '라이브방송',
-    },
-    {
-      code: 'LECTURE_ROOM',
-      name: '강의실',
-    },
-    {
-      code: 'CONFERENCE',
-      name: '컨퍼런스',
-    },
-    {
-      code: 'GYM',
-      name: '운동시설',
-    },
-  ],
-  gathering: [
-    {
-      code: 'PARTY_ROOM',
-      name: '파티룸',
-    },
-    {
-      code: 'PERFORMANCE_HALL',
-      name: '공연장',
-    },
-    {
-      code: 'SHARED_KITCHEN',
-      name: '공유주방',
-    },
-    {
-      code: 'CAFE',
-      name: '카페',
-    },
-    {
-      code: 'SEMINAR_ROOM',
-      name: '세미나실',
-    },
-    {
-      code: 'SMALL_WEDDING',
-      name: '스몰웨딩',
-    },
-    {
-      code: 'GALLERY',
-      name: '갤러리',
-    },
-    {
-      code: 'VIRTUAL_OFFICE',
-      name: '비상주서비스',
-    },
-    {
-      code: 'DAY_CAMPING',
-      name: '당일캠핑',
-    },
-    {
-      code: 'HOUSE',
-      name: '가정집',
-    },
-  ],
-};
-
-type GroupType = keyof typeof dummy;
-
-const GROUP_LABEL = {
-  workspace: '작업 공간',
-  gathering: '모임 공간',
-};
+import { dummy } from '@widgets/space-filter/model/categories.mock.ts';
+import { GROUP_LABEL } from '@widgets/space-filter/config/categories.enum.ts';
+import type { GroupType } from '@widgets/space-filter/model/categories.type.ts';
+import { typedEntries } from '@shared/libs/object.ts';
 
 const SpaceModalContent = ({ onClose }: ModalProps) => {
   return (
     <div className={s.wrapper}>
-      {Object.entries(dummy).map(([key, items]) => {
+      {typedEntries(dummy).map(([key, items]) => {
         const groupName = key as GroupType;
         return (
           <div key={groupName} className={s.group}>
-            <h3 className={s.groupTitle}>{GROUP_LABEL[groupName]}</h3>
+            <h3 className={s.groupTitle}>{GROUP_LABEL[key]}</h3>
             <div className={s.space}>
               {items.map((item) => {
                 const Icon = SPACE_ICON_MAP[item.code];
