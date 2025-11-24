8be64f0c52b441dc9982fa5b089a8e24a2a1a576
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-24 10:04:15 +0900

    ♻️ refactor: api 응답값 space.ts로 이동
    - SearchPage.tsx에서 접근 필요

--- src/shared/configs/space.ts
+++ src/shared/configs/space.ts
@@ -32,3 +32,119 @@ export const SPACE_ICON_MAP: Record<string, SvgIconType> = {
   DAY_CAMPING: Icons.DayCamping,
   HOUSE: Icons.House,
 };
+
+// api 응답값
+export const dummy = {
+  workspace: [
+    {
+      code: 'STUDY_ROOM',
+      name: '스터디룸',
+    },
+    {
+      code: 'MEETING_ROOM',
+      name: '회의실',
+    },
+    {
+      code: 'PRIVATE_OFFICE',
+      name: '독립오피스',
+    },
+    {
+      code: 'COWORKING_OFFICE',
+      name: '코워킹오피스',
+    },
+    {
+      code: 'RENTAL_STUDIO',
+      name: '렌탈스튜디오',
+    },
+    {
+      code: 'PHOTO_STUDIO',
+      name: '촬영스튜디오',
+    },
+    {
+      code: 'HORIZON',
+      name: '호리존',
+    },
+    {
+      code: 'OUTDOOR_SHOOT',
+      name: '실외촬영',
+    },
+    {
+      code: 'RECORDING_ROOM',
+      name: '녹음실',
+    },
+    {
+      code: 'PRACTICE_ROOM',
+      name: '연습실',
+    },
+    {
+      code: 'VOCAL_PRACTICE',
+      name: '보컬연습실',
+    },
+    {
+      code: 'INSTRUMENT_PRACTICE',
+      name: '악기연습실',
+    },
+    {
+      code: 'DANCE_PRACTICE',
+      name: '댄스연습실',
+    },
+    {
+      code: 'LIVE_STREAM',
+      name: '라이브방송',
+    },
+    {
+      code: 'LECTURE_ROOM',
+      name: '강의실',
+    },
+    {
+      code: 'CONFERENCE',
+      name: '컨퍼런스',
+    },
+    {
+      code: 'GYM',
+      name: '운동시설',
+    },
+  ],
+  gathering: [
+    {
+      code: 'PARTY_ROOM',
+      name: '파티룸',
+    },
+    {
+      code: 'PERFORMANCE_HALL',
+      name: '공연장',
+    },
+    {
+      code: 'SHARED_KITCHEN',
+      name: '공유주방',
+    },
+    {
+      code: 'CAFE',
+      name: '카페',
+    },
+    {
+      code: 'SEMINAR_ROOM',
+      name: '세미나실',
+    },
+    {
+      code: 'SMALL_WEDDING',
+      name: '스몰웨딩',
+    },
+    {
+      code: 'GALLERY',
+      name: '갤러리',
+    },
+    {
+      code: 'VIRTUAL_OFFICE',
+      name: '비상주서비스',
+    },
+    {
+      code: 'DAY_CAMPING',
+      name: '당일캠핑',
+    },
+    {
+      code: 'HOUSE',
+      name: '가정집',
+    },
+  ],
+};
