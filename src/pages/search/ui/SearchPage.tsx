4335977a090f477bd67cbdf9350d4f58052c0434
임지수 <dlawltn0708@naver.com>
2025-11-25 00:47:45 +0900

    ✨ feat: 리스트뷰 서브타이틀 구현

--- src/pages/search/ui/SearchPage.tsx
+++ src/pages/search/ui/SearchPage.tsx
@@ -1,3 +1,4 @@
+import SubTitle from '@shared/ui/subTitle/SubTitle';
 import FilterMapButtons from '@widgets/filter/ui/data-filter/button/FilterMapButtons.tsx';
 
 const SearchPage = () => {
@@ -8,11 +9,15 @@ const SearchPage = () => {
         <FilterMapButtons />
       </div>
 
-      {/*프리미엄존 섹션입니다*/}
-      <div>프리미엄프리미엄</div>
+      {/*프리미엄존 섹션*/}
+      <SubTitle variant='large'>
+        <SubTitle.AdBadge>프리미엄존</SubTitle.AdBadge>
+      </SubTitle>
 
-      {/*플러스존 섹션입니다.*/}
-      <div>플러시존플러스</div>
+      {/*플러스존 섹션*/}
+      <SubTitle variant='large'>
+        <SubTitle.AdBadge iconGap='tight'>플러스존</SubTitle.AdBadge>
+      </SubTitle>
 
       {/* 공간 목록 영역입니다. */}
       <div>공간공간공간</div>
