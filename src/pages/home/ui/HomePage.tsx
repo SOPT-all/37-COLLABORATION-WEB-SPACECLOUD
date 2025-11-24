13d6ac2b9fa853270c446dccec811de08d63c6aa
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-24 10:51:15 +0900

    ✨ feat: main filter 배치

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,16 +1,67 @@
+import MainFilter from '@/widgets/space-filter/ui/MainFilter';
+import Button from '@/shared/ui/Button';
+import { useEffect, useState } from 'react';
+import type { FilterKey, FilterState, FilterValue } from '@/widgets/space-filter/ui/MainFilter';
+import { useNavigate } from 'react-router';
+
 /**
  * 페이지의 전체적인 구조만을 잡았습니다.
  * 의미없는 div보다는
  * 시멘틱 태그를 이용해 의미를 부여해주시면 되겠습니다.
  */
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  // 필터 선택 값 관리
+  const [filter, setFilter] = useState<FilterState>({
+    filter: null,
+    location: null,
+    capacity: null,
+    reservationDate: null,
+  });
+
+  // 모달 내부 component로 넘길 필터값 업데이트 함수
+  const handleFilterChange = (key: FilterKey, value: FilterValue) => {
+    setFilter((prev) => ({ ...prev, [key]: value }));
+  };
+
+  // 파라미터 설정 및 리스트 페이지로 이동
+  const handleSearch = () => {
+    const params = new URLSearchParams();
+
+    Object.entries(filter).forEach(([key, value]) => {
+      if (value) {
+        if (typeof value === 'object') {
+          if (value.key) {
+            params.append(key, value.key);
+          } else return;
+        } else {
+          params.append(key, value);
+        }
+      }
+    });
+
+    navigate(`/search?${params.toString()}`);
+  };
+
+  const disabled = Object.values(filter).every((value) => value === null);
+
+  useEffect(() => {
+    console.log('필터값: ', filter);
+  }, [filter]);
+
   return (
     <>
       {/* 메인 페이지 타이틀 영역입니다. */}
       <h1>작업부터 모임까지 어쩌구 저쩌구</h1>
 
       {/* 4가지 필터 섹션 입니다. */}
-      <div>필터</div>
+      <MainFilter filter={filter} onFilterChange={handleFilterChange}>
+        <Button styleType='search' font='body_m_18' onClick={handleSearch} disabled={disabled}>
+          검색
+        </Button>
+      </MainFilter>
 
       {/* 토클 필터 섹션 */}
       <div>토클 필터입니닷</div>
--- src/pages/search/ui/SearchPage.tsx
+++ src/pages/search/ui/SearchPage.tsx
@@ -1,10 +1,59 @@
+import { useSearchParams } from 'react-router';
+import MainFilter, { type FilterKey, type FilterValue } from '@/widgets/space-filter/ui/MainFilter';
+import { AREA_FILTERS } from '@/shared/configs/region';
 import FilterMapButtons from '@widgets/filter/ui/FilterMapButtons.tsx';
+import { dummy } from '@/shared/configs/space';
 
 const SearchPage = () => {
+  const [params, setParams] = useSearchParams();
+  const areaKey = params.get('location');
+  const areaValue = areaKey ? AREA_FILTERS.find((item) => item.key === areaKey) || null : null;
+  const spaceKey = params.get('filter');
+  const findSpaceValue = (code: string) => {
+    for (const space of Object.values(dummy)) {
+      const rst = space.find((item) => item.code === code);
+      if (rst) return rst.name;
+    }
+    return null;
+  };
+  const spaceValue = spaceKey ? findSpaceValue(spaceKey) : null;
+
+  const curFilter = {
+    filter: spaceValue,
+    location: areaValue,
+    capacity: params.get('capacity'),
+    reservationDate: params.get('reservationDate'),
+  };
+
+  console.log(curFilter, 'ii');
+
+  const handleChange = (key: FilterKey, value: FilterValue) => {
+    const newParams = new URLSearchParams(params);
+
+    if (value) {
+      if (typeof value === 'object') {
+        // 지역 필터에서 key가 null이면 서울 전체가 저장된 상태라 URL에서 삭제 필요
+        if (value.key === null) {
+          newParams.delete(key);
+          return;
+        }
+        newParams.set(key, value.key);
+      } else {
+        newParams.set(key, value);
+      }
+    } else {
+      newParams.delete(key);
+    }
+
+    setParams(newParams);
+  };
   return (
     <>
       {/* 필터 섹션입니다. */}
       <div>
+        <MainFilter filter={curFilter} onFilterChange={handleChange}>
+          <FilterMapButtons />
+        </MainFilter>
         <FilterMapButtons />
       </div>
 
