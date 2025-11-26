ee7634c0055870c36c0871c08b9761d93330bbe7
Tnalxmsk <alstnwkd990@naver.com>
2025-11-26 21:40:46 +0900

    :art: style: 올바른 마진 적용

--- src/pages/search/ui/SearchPage.css.ts
+++ src/pages/search/ui/SearchPage.css.ts
@@ -3,7 +3,7 @@ import { spaceToken } from '@/shared/styles/tokens';
 
 export const dividerSpacing = styleVariants({
   top: {
-    margin: `${spaceToken.s24} 0`,
+    margin: `${spaceToken.s20} 0 ${spaceToken.s24} 0`,
   },
   bottom: {
     margin: `${spaceToken.s28} 0`,
