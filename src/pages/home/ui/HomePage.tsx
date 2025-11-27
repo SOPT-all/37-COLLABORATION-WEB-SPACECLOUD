1397dc282ab18e43e6f6d96e3c9fb5fe8b977186
bhyerain <seunghye0307@naver.com>
2025-11-27 15:50:33 +0900

    🔧chore: HomePage 삽입으로 변경

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -10,6 +10,7 @@ import { FetchBoundary } from '@/shared/ui/boundary/FetchBoundary';
 import NewPlaceSection from '@widgets/space-card/ui/new/NewPlaceSection.tsx';
 import * as S from '@pages/home/ui/HomePage.css';
 import Spacer from '@shared/ui/spacer/Spacer.tsx';
+import ChatFloat from '@/widgets/main-view/ui/chat-float/ChatFloat';
 
 const HomePage = () => {
   return (
@@ -57,6 +58,7 @@ const HomePage = () => {
       <FetchBoundary>
         <ReviewSection />
       </FetchBoundary>
+      <ChatFloat />
     </>
   );
 };
