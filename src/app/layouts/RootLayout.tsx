ee53ee8ae2faa1447d1da63d90e5bf2bf7183b61
bhyerain <seunghye0307@naver.com>
2025-11-27 15:47:56 +0900

    ✨feat: 루트 레이아웃에 삽입

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router';
 import { rootLayout } from '@app/layouts/RootLayout.css.ts';
 import Header from '@/widgets/main-view/ui/Header/Header';
 import Footer from '@/widgets/main-view/ui/Footer/Footer';
+import ChatFloat from '@/widgets/main-view/ui/chat-float/ChatFloat';
 
 const RootLayout = () => {
   return (
@@ -10,6 +11,7 @@ const RootLayout = () => {
       {/* 추후 루트 레이아웃 스타일링을 진행합니다. */}
       <main className={rootLayout}>
         <Outlet />
+        <ChatFloat />
       </main>
       <Footer />
     </>
